189a21e5602fe5ad7f8dc70d7cb55e08fe895d62
DESKTOP-DSKRIOJ\Asus <glasnost_inter@yahoo.com>
2021-04-14 02:34:23 +0700

    commit kedua

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +13,15 @@ import java.util.List;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getStudents() {
-        return  List.of(
-                new Student(
-                        1L,
-                        "Azam Mutawakil Farid",
-                        "azam.mf@gmail.com",
-                        LocalDate.of(2014, Month.SEPTEMBER, 12),
-                        6
-
-                )
-        );
+        return studentService.getStudents();
     }
 }
